a7aa5d41fa0bb6cfa08c0bdd9136c1d135570a88
Brice Djilo <dbrice@uno.edu>
2018-06-21 14:27:50 -0600

    Set up DB params

--- config/database.php
+++ config/database.php
@@ -41,9 +41,9 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'HOST'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'CLEARDB_DATABASE_URL'),
+            'database' => env('DB_DATABASE', 'DATABASE'),
             'username' => env('DB_USERNAME', 'USER'),
             'password' => env('DB_PASSWORD', 'PASSWORD'),
             'unix_socket' => env('DB_SOCKET', ''),
